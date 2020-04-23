d1f6f4e67965c7935672b934b671fc29bd3ef3c7
achi <alloc89@mail.ru>
2020-04-24 00:19:47 +0300

    allure + api

---
+++ restThink/src/test/java/allureUse/BodyToAllureTest.java
@@ -0,0 +1,43 @@
+package allureUse;
+
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static utils.Constants.*;
+
+public class BodyToAllureTest {
+    RequestSpecification requestSpec = new RequestSpecBuilder()
+            .setBaseUri(REQRES_BASE_URL)
+            .setContentType(ContentType.ANY.JSON)
+            .setBasePath("/api/users")
+            .build();
+
+    @BeforeTest
+    public void setFilter() {
+        RestAssured.filters(new AllureRestAssured());
+    }
+
+    @Test
+    public void bodyTest() {
+        User user = User.builder()
+                .name("Oleg")
+                .job("Automation")
+                .build();
+
+        given()
+                .spec(requestSpec)
+                .body(user)
+                .when()
+                .post()
+                .then()
+                .log().body()
+                .body("name", equalTo("Oleg"));
+    }
+}
---
+++ restThink/src/test/java/utils/Constants.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Constants {
+
+    public static String REQRES_BASE_URL = "https://reqres.in";
+}
