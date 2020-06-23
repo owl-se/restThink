f5bdba9dec6cb0f9599c9299845499cff2e1f194
achi <alloc89@mail.ru>
2020-06-23 13:55:46 +0300

    performance

---
+++ restThink/src/test/java/performance/PerformanceTest.java
@@ -0,0 +1,29 @@
+package performance;
+
+import org.testng.annotations.Test;
+
+import static performance.ChartCreator.toLineChartPict;
+
+public class PerformanceTest {
+
+    @Test
+    public void performanceTest() throws InterruptedException {
+        int threadNumber = 100;
+        List<Thread> threads = new ArrayList<>();
+        for (int i = 0; i < threadNumber; i++) {
+            RestAssuredThread request = new RestAssuredThread(i);
+            Thread thread = new Thread(request);
+            thread.start();
+            threads.add(thread);
+        }
+
+        for (Thread th : threads) {
+            th.join();
+        }
+
+        System.out.println(RestAssuredThread.times);
+        toLineChartPict("Endpoint performance", "Time Of Responses", RestAssuredThread.times);
+
+        assertEquals(String.format("There is %s unsuccessful responses", RestAssuredThread.failures), 0, RestAssuredThread.failures);
+    }
+}
---
+++ restThink/src/test/java/performance/RestAssuredThread.java
@@ -0,0 +1,31 @@
+package performance;
+
+public class RestAssuredThread extends Thread {
+
+    private int threadNumber;
+    public static Map<Integer, Long> times;
+    public static int failures;
+
+    static {
+        times = Collections.synchronizedMap(new HashMap<>());
+    }
+
+    public RestAssuredThread(int numb) {
+        threadNumber = numb;
+    }
+
+    public void run() {
+        System.out.println("start thread " + threadNumber);
+        ValidatableResponse response = given()
+                .baseUri("https://reqres.in")
+                .contentType(ContentType.JSON)
+                .basePath("/api/users")
+                .param("page", 2)
+                .when()
+                .get()
+                .then();
+        if(response.extract().statusCode() != 200)
+            failures++;
+        times.put(threadNumber, response.extract().time());
+    }
+}
